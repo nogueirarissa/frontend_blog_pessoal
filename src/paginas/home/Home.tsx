c7c52782214d65474c5131ddbbf941d692dfb5d9
nogueirarissa <larissa.nogueiradm@gmail.com>
2022-09-29 17:23:36 -0300

    Cria blog pessoal no React

---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Home.css";
+
+function Home(){
+    return (
+    <>
+        <h1 className="titulo">Home</h1>
+        <img className="img" src="https://i.imgur.com/wU1enY4.jpeg" alt="Imagem Tela Inicial" />
+    </>
+    );
+}
+
+export default Home;
